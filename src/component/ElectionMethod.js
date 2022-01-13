ab57a89f0ace25ec23f803361b3e2bf035a58278
Ramirisu <labyrinth.ramirisu@gmail.com>
2022-01-13 22:12:54 +0800

    Add selection for selecting election model

--- src/component/ElectionMethod.js
+++ src/component/ElectionMethod.js
@@ -3,17 +3,25 @@ import { electionModelTaiwan2008, electionModelGermany } from './electionModel';
 import { Table } from "./Table";
 import TW2020 from './tw2020.json';
 
+
+const ELECTION_MODELS = [
+    {
+        name: 'Taiwan (2008 Version)',
+        handler: electionModelTaiwan2008
+    },
+    {
+        name: 'Germany',
+        handler: electionModelGermany
+    },
+];
+
 export const ElectionMethod = () => {
 
     const TOTAL_SEATS = 113;
     const QUALIFIED_THREASHOLD = 0.05;
 
-    const electionModelFunc = electionModelTaiwan2008;
-
-    const initialData = useMemo(() => TW2020, []);
-    electionModelFunc(initialData, TOTAL_SEATS, QUALIFIED_THREASHOLD);
-    const [data, setData] = React.useState(initialData);
-
+    const [electionModelIndex, setElectionModelIndex] = React.useState(0);
+    const [data, setData] = React.useState(ELECTION_MODELS[electionModelIndex].handler(useMemo(() => TW2020, []), TOTAL_SEATS, QUALIFIED_THREASHOLD));
 
     const updateData = (rowIndex, columnId, value) => {
         setData(old =>
@@ -27,10 +35,21 @@ export const ElectionMethod = () => {
                 return row;
             })
         );
-        setData(old => electionModelFunc(old, TOTAL_SEATS, QUALIFIED_THREASHOLD));
+        setData(old => ELECTION_MODELS[electionModelIndex].handler(old, TOTAL_SEATS, QUALIFIED_THREASHOLD));
     }
 
     return (<div>
+        <select onChange={e => {
+            const index = e.target.value;
+            setElectionModelIndex(index);
+            setData(old => ELECTION_MODELS[index].handler(old, TOTAL_SEATS, QUALIFIED_THREASHOLD));
+        }}>
+            {ELECTION_MODELS.map((obj, index) => (
+                <option key={index} value={index}>
+                    {obj.name}
+                </option>
+            ))}
+        </select>
         <Table
             data={data}
             updateData={updateData}
