b771aff1763b3cd01d736eed6e94e022d024ae0b
Ramirisu <labyrinth.ramirisu@gmail.com>
2022-01-18 03:41:58 +0800

    Fix test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Electoral System</h1>
+        <h1>Electoral Systems</h1>
       </header>
       <ElectoralSystem />
     </div>
